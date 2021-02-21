0f6e9f2f3887c7be256fed3969269177906c75bc
neeraj suri <neeraj.suri@yahoo.com>
2021-02-21 17:33:45 +0530

    functions

--- functionsex.py
+++ functionsex.py
@@ -1,1 +1,9 @@
-functionsex.py
+def sum_two_numbers(number1, number2):
+	print("sum_two_numbers is:", number1+number2)
+
+sum_two_numbers	(10,15)
+
+def is_inside_bar(yesterday_high,yesterday_low, today_high,today_low):
+		if(yesterday_high > today_high and yesterday_low < today_low):
+			print("ïnside bar confirmed")
+is_inside_bar (252,210 , 250, 215)
